3c100c4aac1b3a367a100241695ea451f16752e3
Mohammed Ismail <mohammedismail72425@gmail.com>
2025-04-22 17:47:11 +0530

    updated sign-in/page.tsx

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,34 +1,22 @@
 import { SignIn } from "@clerk/nextjs";
+import Image from "next/image";
 import React from "react";
 
 export default function page() {
   return (
-    <section className="h-100 gradient-form">
-      <div className="container py-5 h-100">
-        <div className="row d-flex justify-content-center align-items-center h-100">
-          <div className="col-xl-10">
-            <div className="card rounded-3 text-black">
-              <div className="row g-0">
-                <div className="col-lg-6">
-                  <div className="card-body p-md-5 mx-md-4">
-                    <div className="text-center">
-                      <h4 className="mt-1 mb-5 pb-1">We are The Lotus Team</h4>
-                    </div>
-                  </div>
-                </div>
-                <div className="col-lg-6 d-flex align-items-center gradient-custom-2">
-                  <div className="text-white px-3 py-4 p-md-5 mx-md-4">
-                    <h4 className="mb-4">We are more than just a company</h4>
-                    <p className="small mb-0">
-                      Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-                      sed do eiusmod tempor incididunt ut labore et dolore magna
-                      aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                      ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
+    <section>
+      <div className="grid grid-cols-1 lg:grid-cols-2 h-screen">
+        <div className="hidden lg:flex md:col-span-1 justify-center items-center h-full pb-20">
+          <Image
+            src={"/authentication-image.svg"}
+            width={500}
+            height={100}
+            alt="Authentication Image"
+          />
+        </div>
+        <div className="col-span-1">
+          <div className="flex justify-center items-center h-full pb-20 hover:scale-105 duration-300">
+            <SignIn />
           </div>
         </div>
       </div>
