9cdba22d041ba0b91137df9422b99f395ec8e2b4
Mohammed Ismail <mohammedismail72425@gmail.com>
2025-04-22 20:31:05 +0530

    updated SideNav.tsx

--- app/dashboard/_components/SideNav.tsx
+++ app/dashboard/_components/SideNav.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { Progress } from "@/components/ui/progress";
 import {
   ChartNoAxesCombined,
   LibraryBig,
@@ -60,7 +61,7 @@ export default function SideNav() {
           <h2
             key={index}
             className={`flex items-center gap-3 p-4 mb-2 text-gray-700 hover:bg-modern hover:text-white rounded-lg cursor-pointer ${
-              path === menu.path && "bg-modern text-white"
+              path === menu.path && "bg-modern text-white scale-105"
             }`}
           >
             <menu.icon />
@@ -68,8 +69,17 @@ export default function SideNav() {
           </h2>
         ))}
       </div>
-      <div className="fixed bottom-20 p-6 w-64">
-        <Button className="w-full"><Sparkles /> Create Form</Button>
+      <div className="fixed bottom-6 p-6 w-64">
+        <Button className="w-full">
+          <Sparkles className="w-4 h-4" /> Generate Form
+        </Button>
+        <div className="my-7">
+          <Progress value={33} />
+          <h2 className="mt-2 text-gray-600">
+            <strong>2</strong> out of <strong>3</strong> Credits Used
+          </h2>
+          <h2 className="mt-3 text-center bg-gradient-to-r from-purple-200 to-blue-200 hover:from-blue-200 hover:to-purple-200 px-2 py-1 text-gray-600 rounded-md">Upgrade to generate more</h2>
+        </div>
       </div>
     </div>
   );
