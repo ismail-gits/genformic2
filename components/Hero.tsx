a0419f9d39b4cefd31f38de98f3da4c89560307c
Mohammed Ismail <mohammedismail72425@gmail.com>
2025-04-22 14:57:40 +0530

    updated Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Button } from "./ui/button";
+import { Sparkles } from "lucide-react";
 
 function Hero() {
   return (
@@ -7,20 +8,23 @@ function Hero() {
       <div className="mx-auto w-screen max-w-screen-xl px-4 py-16 sm:px-6 sm:py-24 lg:px-8 lg:py-32">
         <div className="mx-auto max-w-prose text-center">
           <h1 className="text-4xl font-bold text-gray-900 sm:text-5xl">
-            Understand user flow and
-            <strong className="text-modern"> increase </strong>
-            conversions
+            Unlock the Future of
+            <strong className="text-modern"> Form Creation with AI. </strong>
           </h1>
 
-          <p className="mt-4 text-base text-pretty text-gray-700 sm:text-lg/relaxed">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque,
-            nisi. Natus, provident accusamus impedit minima harum corporis
-            iusto.
+          <p className="mt-4 text-base text-pretty text-gray-500 sm:text-lg/relaxed">
+            Genformic uses AI to create, share, and automate forms effortlessly.
+            Just provide a prompt, and let AI handle the rest.
           </p>
 
           <div className="mt-4 flex justify-center gap-4 sm:mt-6">
-            <Button size={"lg"}>Get Started</Button>
-            <Button variant={"outline"} size={"lg"}>Learn More</Button>
+            <Button size={"lg"} className="flex justify-center items-center">
+              <Sparkles />
+              <div>Generate AI Form</div>
+            </Button>
+            <Button variant={"outline"} size={"lg"}>
+              Learn More
+            </Button>
           </div>
         </div>
       </div>
