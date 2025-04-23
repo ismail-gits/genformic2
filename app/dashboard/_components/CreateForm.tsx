97c99f3d60158dabdff894a118d2beaf6a970ab6
Mohammed Ismail <mohammedismail72425@gmail.com>
2025-04-23 10:38:32 +0530

    updated CreateForm.tsx

--- app/dashboard/_components/CreateForm.tsx
+++ app/dashboard/_components/CreateForm.tsx
@@ -1,45 +1,58 @@
+"use client";
+
+import { Sparkles } from "lucide-react";
+import { useState } from "react";
+
 import { Button } from "@/components/ui/button";
 import {
   Dialog,
   DialogContent,
   DialogDescription,
-  DialogFooter,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
 import { Textarea } from "@/components/ui/textarea";
-import { Sparkles } from "lucide-react";
-import React, { useState } from "react";
 
 export default function CreateForm() {
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
   const [userInput, setUserInput] = useState("");
 
+  const onGenerateForm = (): void => {
+    console.log(userInput);
+    setIsDialogOpen(false);
+  };
+
   return (
-    <Dialog>
-      <DialogTrigger asChild>
-        <Button>
-          <Sparkles /> Generate Form
-        </Button>
-      </DialogTrigger>
-      <DialogContent>
-        <DialogHeader>
-          <DialogTitle className="font-bold">Generate New Form</DialogTitle>
-          <DialogDescription>
-            <Textarea
-              onChange={(e) => {
-                setUserInput(e.target.value);
-              }}
-              className="my-2 text-black"
-              placeholder="Type your prompt: e.g., 'Job application form for a startup'"
-            />
-            <div className="flex gap-2 my-3 justify-end">
-              <Button variant={"destructive"}>Cancel</Button>
-              <Button>Generate</Button>
+    <div>
+      <Button onClick={() => setIsDialogOpen(true)}>
+        <Sparkles /> Generate Form
+      </Button>
+      <Dialog open={isDialogOpen}>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle className="font-bold">Generate New Form</DialogTitle>
+            <DialogDescription>
+              <Textarea
+                onChange={(e) => {
+                  setUserInput(e.target.value);
+                }}
+                className="my-2 text-black"
+                placeholder="Type your prompt: e.g., 'Job application form for a startup'"
+              />
+            </DialogDescription>
+            <div className="flex gap-2 justify-end">
+              <Button
+                variant={"destructive"}
+                onClick={() => setIsDialogOpen(false)}
+              >
+                Cancel
+              </Button>
+              <Button onClick={onGenerateForm}>Generate</Button>
             </div>
-          </DialogDescription>
-        </DialogHeader>
-      </DialogContent>
-    </Dialog>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+    </div>
   );
 }
