0c21718ab97e1a4214c0713553b0f16f087d7c6f
Mohammed Ismail <mohammedismail72425@gmail.com>
2025-04-26 08:36:02 +0530

    updated FormUi.tsx for all component of form

--- app/form/edit/[formId]/page.tsx
+++ app/form/edit/[formId]/page.tsx
@@ -33,15 +33,15 @@ export default function EditForm() {
   }, [params.formId]);
 
   return (
-    <div className="p-6">
-      <h2 className="flex items-center gap-2 my-5 hover:font-bold hover:text-lg transition">
+    <div className="px-6 ">
+      <h2 className="flex items-center gap-2 my-6 hover:font-bold hover:text-lg transition">
         <Button variant={"outline"} className="rounded-4xl hover:scale-105">
           <ArrowLeft className="text-purple-600"/> Back
         </Button>
       </h2>
       <div className="grid grid-col-1 md:grid-cols-3 gap-5">
         <div className="p-5 border rounded-lg shadow">Controller</div>
-        <div className="md:col-span-2 border rounded-lg p-5 h-screen flex items-center justify-center"><FormUi form={form}/></div>
+        <div className="md:col-span-2 border rounded-lg p-10 min-h-screen flex items-center justify-center"><FormUi form={form}/></div>
       </div>
     </div>
   );
--- app/form/edit/_components/FormUi.tsx
+++ app/form/edit/_components/FormUi.tsx
@@ -1,21 +1,96 @@
-import { FieldSchemaType, FormSchemaType } from '@/lib/zod'
-import { Loader } from 'lucide-react'
-import React from 'react'
+import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Textarea } from "@/components/ui/textarea";
+import { FieldSchemaType, FormSchemaType, OptionSchemaType } from "@/lib/zod";
+import { Loader } from "lucide-react";
+import React from "react";
 
 type FormUiType = {
-  form: FormSchemaType | null
-}
+  form: FormSchemaType | null;
+};
 
-export default function FormUi({form}: FormUiType) {
-  
+export default function FormUi({ form }: FormUiType) {
   if (!form) {
-    return <div><Loader className='animate-spin'/></div>
+    return (
+      <div>
+        <Loader className="animate-spin text-purple-600"/>
+      </div>
+    );
   }
 
   return (
-    <div className='border p-5'>
-      <h2 className='font-bold text-center text-2xl'>{form.title}</h2>
-      <h2 className='text-sm text-gray-500 text-center'>{form.subheading}</h2>
+    <div className="border p-5 md:w-[600] overflow-y-auto rounded-md">
+      <h2 className="font-bold text-center text-2xl">{form.title}</h2>
+      <h2 className="text-sm text-gray-500 text-center">{form.subheading}</h2>
+
+      {form.fields.map((field: FieldSchemaType, index: number) => (
+        <div key={index}>
+          <div className="my-3">
+            {field.type === "select" ? (
+              <div>
+                <Label className="text-sm text-gray-600">{field.label}</Label>
+                <Select required={field.required}>
+                  <SelectTrigger className="w-full">
+                    <SelectValue placeholder={field.label} />
+                  </SelectTrigger>
+                  <SelectContent>
+                    {field.options?.map(
+                      (option: OptionSchemaType, index: number) => (
+                        <SelectItem key={index} value={option.value}>
+                          {option.label}
+                        </SelectItem>
+                      )
+                    )}
+                  </SelectContent>
+                </Select>
+              </div>
+            ) : field.type === "checkbox" ? (
+              <div className="flex items-center gap-2">
+                <Checkbox id={field.name} required={field.required} />
+                <Label className="text-sm text-gray-600">{field.label}</Label>
+              </div>
+            ) : field.type === "radio" ? (
+              <RadioGroup required={field.required}>
+                <Label className="text-sm text-gray-600">{field.label}</Label>
+                {field.options?.map(
+                  (option: OptionSchemaType, index: number) => (
+                    <div className="flex items-center gap-2" key={index}>
+                      <RadioGroupItem value={option.value} />
+                      <Label className="text-sm text-gray-600">
+                        {option.label}
+                      </Label>
+                    </div>
+                  )
+                )}
+              </RadioGroup>
+            ) : field.type === "textarea" ? (
+              <div>
+                <Label className="text-sm text-gray-600">{field.label}</Label>
+                <Textarea placeholder={field.placeholder} name={field.name} required={field.required}/>
+              </div>
+            ) : (
+              <div>
+                <Label className="text-sm text-gray-600">{field.label}</Label>
+                <Input
+                  type={field.type}
+                  placeholder={field.placeholder}
+                  name={field.name}
+                  required={field.required}
+                />
+              </div>
+            )}
+          </div>
+        </div>
+      ))}
     </div>
-  )
+  );
 }
