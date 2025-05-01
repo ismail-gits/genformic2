8179b8b8f9d9bf5bbcdc71e81a98d518b640363c
Mohammed Ismail <mohammedismail72425@gmail.com>
2025-05-02 01:07:42 +0530

    added Genformic watermark for free users

--- app/form/live/[formId]/page.tsx
+++ app/form/live/[formId]/page.tsx
@@ -3,15 +3,31 @@
 import { formBackgroundAtom } from "@/app/store/atoms/formBackgroundAtom";
 import FormUi from "../../edit/_components/FormUi";
 import { useAtom } from "jotai";
+import Link from "next/link";
+import Image from "next/image";
 
 export default function LiveForm() {
-  const [selectedBackground] = useAtom(formBackgroundAtom)
+  const [selectedBackground] = useAtom(formBackgroundAtom);
 
   return (
-    <div className="p-10 flex justify-center items-center"
-      style={{backgroundImage: selectedBackground}}
+    <div
+      className="p-10 flex justify-center items-center"
+      style={{ backgroundImage: selectedBackground }}
     >
-      <FormUi mode="live"/>
+      <FormUi mode="live" />
+      <Link href={"/"} target="_blank">
+        <div className="flex gap-2 items-center bg-black rounded-2xl px-3 py-1 fixed bottom-5 left-20 hover:scale-105 transition-all duration-200">
+          <Image
+            src={"/G-Logo.svg"}
+            height={24}
+            width={24}
+            alt="Genformi-Logo"
+          />
+          <h2 className="font-bold text-modern cursor-pointer">
+            Generate Form With AI.
+          </h2>
+        </div>
+      </Link>
     </div>
   );
 }
