40ee5f36e355bd9e1d7dcec115045991aedd49d5
Mohammed Ismail <mohammedismail72425@gmail.com>
2025-05-15 00:22:24 +0530

    addes eslint.config.js

---
+++ eslint.config.ts
@@ -0,0 +1,17 @@
+import eslintPlugin from '@eslint/js';
+
+export default [
+  {
+    ignores: ['lib/prisma/**'],
+  },
+  eslintPlugin.configs.recommended,
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: '@typescript-eslint/parser',
+    },
+    rules: {
+      // your custom rules
+    },
+  },
+];
